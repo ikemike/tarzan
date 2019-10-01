af7f1b46daec1ed3237d536dd45d06a92922646c
Lemke <Isaac.Lemke@covance.com>
2019-10-01 12:28:15 -0500

    Included additional config settings

--- src/app/tarzan-config.ts
+++ src/app/tarzan-config.ts
@@ -1,4 +1,5 @@
 export class TarzanConfig {
+    
     searchSettings = {
         productName:      'Note 10',
         currencySymbol:   '$',
@@ -14,7 +15,23 @@ export class TarzanConfig {
         validHighPrice: 1000, 
         maxDistance: parseInt(this.searchSettings.maxDistance)
       };
+    /*
+    searchSettings = {
+      productName:      'Nanoleaf',
+      currencySymbol:   '$',
+      city:             'madison',    // Your city : Used in FB Marketplace queries 
+      localZip:         '53716',      // Your local zip code. Used in Ebay queries. 
+      maxDistance:      '200'          // How close the product has to be (in miles). Used in Ebay queries. 
+    };
 
+    validationSettings = {
+      productMustContainWord: 'leaf',
+      productMustContainAtLeastOneWord: ['nanoleaf', 'nano', 'leaf', 'light', 'panel'],
+      validLowPrice: 50,
+      validHighPrice: 300, 
+      maxDistance: parseInt(this.searchSettings.maxDistance)
+    };
+    */
       sitesToReview = [
          `https://www.ebay.com/sch/i.html?_nkw=${encodeURI(this.searchSettings.productName)}&_stpos=${this.searchSettings.localZip}&_sadis=${this.searchSettings.maxDistance}&_fspt=1`
         ,`https://mobile.facebook.com/marketplace/${this.searchSettings.city}/?radius_in_km=17&query=${encodeURI(this.searchSettings.productName)}`
@@ -23,6 +40,8 @@ export class TarzanConfig {
         //,`https://www.walmart.com/search/?cat_id=0&query=${encodeURI(this.searchSettings.productName)}`
         //,`https://us.letgo.com/en/scl/wisconsin/dane-county/${this.searchSettings.city}?searchTerm=${encodeURI(this.searchSettings.productName)}&price[min]=${this.validationSettings.validLowPrice}&price[max]=${this.validationSettings.validHighPrice}`
       ]
+
+
 
 
 }
